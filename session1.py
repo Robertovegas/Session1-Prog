d64ac9f4ded6b15cfe9c46d389acd4194b82ffb1
RobertoVegas <rob030521@gmail.com>
2026-01-20 16:59:12 +0100

    My first commit ever

---
+++ session1.py
@@ -0,0 +1,14 @@
+print("Hello world")
+print("7+6")
+print(7+6)
+#hello
+print("3 + 4 =",3+4,"and 5 x 6 =", 5*6)
+print("I have $",100*200,".")
+print("I have $",100*200,".",sep="")
+#end is iused to tell print what to do after it prints
+print("1",end=" ")
+print("2",end=" ")
+print("3", end="bob stole my car")
+print("4", end=" ")
+print("5", end=" ")
+
